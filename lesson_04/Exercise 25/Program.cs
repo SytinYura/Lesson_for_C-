58cfc7e51169859bc08ad13a8436c9cd40a76e83
Yura <sytinyura@gmail.com>
2023-02-01 01:18:57 +0400

    запушиваю 4 и 5 дз

--- lesson_04/Exercise 25/Program.cs
+++ lesson_04/Exercise 25/Program.cs
@@ -2,3 +2,22 @@
 //Нельзя использовать библиотеку Math!
 //3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
+
+Console.Write("Введите число A: ");
+int numA = Convert.ToInt32(Console.ReadLine()!);
+Console.Write("Введите число B: ");
+int numB = Convert.ToInt32(Console.ReadLine()!);
+
+int Exponentiation(int numberA, int numberB)
+{
+  int result = 1;
+  for(int i=1; i <= numberB; i++)
+  {
+    result = result * numberA;
+  }
+    // int result = Math.Pow(numberA, numberB);
+    return result;
+}
+
+int exponentiation = Exponentiation(numA, numB);
+Console.WriteLine("Ответ: " + exponentiation);
